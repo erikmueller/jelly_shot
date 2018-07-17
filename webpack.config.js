47ce27a2523ffb0121a84583433a03f9ea028279
Erik Müller <e.mueller@epages.com>
2018-07-17 11:49:59 +0200

    Chore: Reduce blocking resources

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,14 @@ module.exports = {
   context: path.resolve(__dirname, './web/static/'),
   entry: {
     'js/bundle': './js/app.js',
-    'css/bundle': [
-      './styles/style.less',
-      './styles/cv.less',
-      'highlight.js/styles/obsidian.css'
-    ],
+    'css/bundle': ['./styles/style.less', './styles/cv.less'],
+    'css/desktop': './styles/desktop.less',
+    'css/print': './styles/print.less',
     'css/vendor': [
       '@fortawesome/fontawesome-free/less/fontawesome.less',
       '@fortawesome/fontawesome-free/less/fa-brands.less',
-      '@fortawesome/fontawesome-free/less/fa-solid.less'
+      '@fortawesome/fontawesome-free/less/fa-solid.less',
+      'highlight.js/styles/obsidian.css'
     ]
   },
   output: {
