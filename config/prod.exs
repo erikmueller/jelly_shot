b331065bdb755a506f261a6c34e0440901fbc174
Erik Müller <e.mueller@epages.com>
2018-07-18 21:25:03 +0200

    Fix: Manifest config

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :jelly_shot, JellyShot.Endpoint,
   http: [port: {:system, "PORT"}, compress: true],
   url: [scheme: "https", host: "www.slackoverflow.me", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
